257896dfac01b593c52446021f5cdb1bbca635a4
Niger <nigerlittlepoole@tumblr.com>
2015-11-05 08:35:30 -0500

    updated description on package install

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='thermometr',
       version='0.1',
-      description='The funniest joke in the world',
+      description='The Anomaly Detection Package',
       url='https://github.ewr01.tumblr.net/nigerlittlepoole/thermometr',
       author='Niger Little-Poole',
       author_email='nigerlittlepoole@tumblr.com',
