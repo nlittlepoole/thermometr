9a1e3037f11fce445b3d43e5b73f0434d2aadc6e
Niger <nlittlepoole@gmail.com>
2015-12-16 20:14:49 -0500

    added dependencies to setup.py and updated info to be non tumblr specific

--- setup.py
+++ setup.py
@@ -3,9 +3,15 @@ from setuptools import setup
 setup(name='thermometr',
       version='0.1',
       description='The Anomaly Detection Package',
-      url='https://github.ewr01.tumblr.net/nigerlittlepoole/thermometr',
+      url='https://github.com/nlittlepoole/thermometr',
       author='Niger Little-Poole',
-      author_email='nigerlittlepoole@tumblr.com',
+      author_email='nlittlepoole@gmail.com',
       license='MIT',
       packages=['thermometr'],
+      install_requires=[
+        "statsmodels",
+        "numpy",
+        "pandas",
+        "scipy"
+      ],
       zip_safe=False)
